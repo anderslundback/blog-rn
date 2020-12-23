52c437b64afe3043b681cc175deb9d2ae7d899c9
Anders Lundback <lundback.anders@gmail.com>
2020-12-23 16:53:26 +0100

    [feat] Render static context data

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    const blogPosts = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index screen</Text>
-            <Text>{value}</Text>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     )
 };

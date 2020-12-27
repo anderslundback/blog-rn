b95ab1ffadbf8adb7d17c6f6cb8930be3bb26c53
Anders Lundback <lundback.anders@gmail.com>
2020-12-27 14:42:18 +0100

    feat: Navigate to ShowScreen

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/indexScreen';
 import React from 'react';
 import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+
 
 const navigator = createStackNavigator({
-  Index: IndexScreen
+  Index: IndexScreen,
+  Show: ShowScreen
 }, {
   initalRouteName: 'Index',
   defaultNavigationOptions: {
---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ShowScreen = () => {
+    return (
+        <View>
+            <Text>Show screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
+
+export default ShowScreen;

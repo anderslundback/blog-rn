4a2506245304b3c35f834c254a4571230cd09505
Anders Lundback <lundback.anders@gmail.com>
2020-12-28 13:25:06 +0100

    feat: Wire up EditScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const EditScreen = () => {
+    return (
+        <View>
+            <Text>Edit</Text>
+        </View>
+    )
+};
+
+export default EditScreen;
